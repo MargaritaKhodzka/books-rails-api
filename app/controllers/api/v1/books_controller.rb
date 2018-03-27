b61605d586f9deab28c4b3d9e3f5d33afb61a971
MargaritaKhodzka <mkhodzka@gmail.com>
2018-03-27 00:17:12 -0400

    add destroy action to books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -10,6 +10,13 @@ class Api::V1::BooksController < ApplicationController
     render json: @book, status: 201
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      render json: {bookId: book.id}, status: 200
+    end
+  end
+
   private
   def book_params
     params.require(:book).permit(:title)
