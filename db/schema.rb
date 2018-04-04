e7fa5bb3af878f42ef9e94a7a840f486047381e8
MargaritaKhodzka <mkhodzka@gmail.com>
2018-04-03 23:36:53 -0400

    comment model associated with book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403194322) do
+ActiveRecord::Schema.define(version: 20180404033421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,15 @@ ActiveRecord::Schema.define(version: 20180403194322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
-    t.text "comment"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
+  end
+
+  add_foreign_key "comments", "books"
 end
